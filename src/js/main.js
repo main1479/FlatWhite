5af832a6b0818cc6c956830c92898d5f2849a774
main1479 <m.main2402@gmail.com>
2021-01-03 09:59:18 +0600

    init files

---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+$(function(){
+    $('input[type="number"]').niceNumber();
+  });
+
+
+  $(document).ready(function() {
+    $('select').niceSelect();
+  });
+
+
+  $('#checkIn').Zebra_DatePicker();
+  $('#checkOut').Zebra_DatePicker();
